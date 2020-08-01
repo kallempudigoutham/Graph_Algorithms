6c14e82acef38caa8171e9ea98cefa3d7ad80fb8
goutham kallempudi <gouthamkallempudi1996@gmail.com>
2020-08-01 14:44:55 +0530

    Depth First search Bug fix, Finding connected components using dfs

--- ConnectedComponents.py
+++ ConnectedComponents.py
@@ -1,9 +1,11 @@
-def depth_first_search(graph_list, no_of_nodes, visited):
+def no_connected_components(graph_list, no_of_nodes, visited):
     search_order = []
+    count = 0
     for node in range(no_of_nodes):
         if visited[node] is False:
+            count = count + 1
             dfs(graph_list, node, visited, search_order)
-    return search_order
+    return count, search_order
 
 
 def dfs(graph_list, node, visited, search_order):
@@ -13,5 +15,4 @@ def dfs(graph_list, node, visited, search_order):
         if visited[graph_list[node][i]] is False:
             visited[graph_list[node][i]] = True
             dfs(graph_list, graph_list[node][i], visited, search_order)
-
 
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from GraphInitialization import *
 from DepthFirstSearch import depth_first_search
+from ConnectedComponents import no_connected_components
 
-no_of_nodes = 6
+no_of_nodes = 18
 
 graph = initialise_graph(no_of_nodes);
-graph = add_edge(graph, 0, 1)
-graph = add_edge(graph, 0, 5)
-graph = add_edge(graph, 1, 2)
-graph = add_edge(graph, 1, 3)
-graph = add_edge(graph, 3, 4)
+graph = add_edge(graph, 0, 8)
+graph = add_edge(graph, 1, 5)
+graph = add_edge(graph, 2, 9)
+graph = add_edge(graph, 3, 9)
+graph = add_edge(graph, 4, 0)
+graph = add_edge(graph, 5, 16)
+graph = add_edge(graph, 5, 17)
+graph = add_edge(graph, 6, 11)
+graph = add_edge(graph, 7, 6)
+graph = add_edge(graph, 8, 4)
+graph = add_edge(graph, 8, 14)
+graph = add_edge(graph, 9, 15)
+graph = add_edge(graph, 11, 7)
+graph = add_edge(graph, 13, 0)
+graph = add_edge(graph, 14, 0)
+graph = add_edge(graph, 14, 13)
+graph = add_edge(graph, 15, 10)
+graph = add_edge(graph, 15, 2)
 
 
 visited = []
@@ -16,4 +30,13 @@ for i in range(no_of_nodes):
     visited.append(False)
 
 print(graph)
-print(depth_first_search(graph, no_of_nodes, visited))
+depth_first_search_order = depth_first_search(graph, no_of_nodes, visited)
+
+visited = []
+for i in range(no_of_nodes):
+    visited.append(False)
+
+number_of_connected_components, search_order = no_connected_components(graph, no_of_nodes, visited)
+
+print(number_of_connected_components)
+print(search_order)
